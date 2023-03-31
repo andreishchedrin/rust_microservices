22200b373192aa76dc9a9c52f4d6770d32f2a8ba
andreishchedrin <schedrin-post@rambler.ru>
2023-03-31 21:05:31 +0300

    init

---
+++ rust_2/src/main.rs
@@ -0,0 +1,11 @@
+extern crate postgres;
+
+use postgres::{Connection, ConnectParams, ConnectTarget, SslMode, UserInfo};
+
+fn main() {
+    println!("Hello, world!");
+    // let mut client = Client::connect(
+    //     "postgresql://test_user:test_pass@0.0.0.0:5432/test_db",
+    //     NoTls,
+    // )?;
+}
