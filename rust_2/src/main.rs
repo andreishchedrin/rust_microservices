246bf70fdf65e94ab2c1cd777fe6d4763e881aa9
andreishchedrin <schedrin-post@rambler.ru>
2023-04-01 10:15:00 +0300

    pg connect add

--- rust_2/src/main.rs
+++ rust_2/src/main.rs
@@ -3,9 +3,33 @@ extern crate postgres;
 use postgres::{Connection, ConnectParams, ConnectTarget, SslMode, UserInfo};
 
 fn main() {
-    println!("Hello, world!");
-    // let mut client = Client::connect(
-    //     "postgresql://test_user:test_pass@0.0.0.0:5432/test_db",
-    //     NoTls,
-    // )?;
+    println!("Start app");
+     db_init()
+}
+
+fn db_init() {
+    let params = ConnectParams {
+        target: ConnectTarget::Tcp("postgres".to_string()),
+        port: Some(5432),
+        user: Some(UserInfo {
+            user: "test_user".to_string(),
+            password: None,
+        }),
+        database: Some("test_db".to_string()),
+        options: vec![],
+    };
+
+    let conn =
+        Connection::connect(
+            params,
+            &SslMode::None).unwrap();
+
+    conn.execute("CREATE TABLE IF NOT EXISTS messages (
+                       id              SERIAL PRIMARY KEY,
+                       data            VARCHAR NOT NULL,
+                       created_at      TIMESTAMP DEFAULT NOW(),
+                       sent_at         TIMESTAMP
+                   )", &[]).unwrap();
+
+    println!("Database init done!");
 }
